b75d4d21724da8c19553cbf18222c020477c80b5
mawenhao <mawh@bxlsj.com>
2024-07-30 17:22:38 +0800

    feat: supports set env for applicaiton

--- web/src/pages/App/Settings/EnvironmentSettings.tsx
+++ web/src/pages/App/Settings/EnvironmentSettings.tsx
@@ -1,10 +1,15 @@
-import { ProCard } from '@ant-design/pro-components';
+import { EditableProTable, ProCard } from '@ant-design/pro-components';
 
 type EnvironmentSettingsProps = {
   appId: string;
 }
 
+function getNewId() {
+  return (Math.random() * 1000).toFixed(0);
+}
+
 const EnvironmentSettings = ({ appId }: EnvironmentSettingsProps) => {
+
   return (
     <ProCard
       title={'变量配置'}
@@ -12,7 +17,53 @@ const EnvironmentSettings = ({ appId }: EnvironmentSettingsProps) => {
       collapsible
       bordered
     >
-      <div>EnvironmentSettings</div>
+      <EditableProTable
+        rowKey="id"
+        recordCreatorProps={{
+          newRecordType: 'dataSource',
+          position: 'bottom',
+          record: () => ({ id: getNewId() }),
+        }}
+        columns={[
+          {
+            title: '变量名',
+            dataIndex: 'name',
+            formItemProps: {
+              rules: [
+                {
+                  required: true,
+                  message: '变量名为必填项',
+                },
+              ],
+            },
+          },
+          {
+            title: '变量值',
+            dataIndex: 'value',
+            valueType: 'input',
+            formItemProps: {
+              rules: [
+                {
+                  required: true,
+                  message: '变量值为必填项',
+                },
+              ],
+            },
+          },
+          {
+            title: '操作',
+            valueType: 'option',
+            width: 200,
+            render: (text, record, _, action) => {
+              return [
+                <a key="editable" onClick={() => {
+                  action.startEditable?.(record.id);
+                }}>编辑</a>,
+              ];
+            },
+          },
+        ]}
+      />
     </ProCard>
   );
 };
