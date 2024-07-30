d31ef5cbb65e80ceaf2cc3fcec7e9252cbd5f5a3
mawenhao <mawh@bxlsj.com>
2024-07-30 20:39:21 +0800

    feat: support online debug

--- web/src/components/Script/JavaScript/index.tsx
+++ web/src/components/Script/JavaScript/index.tsx
@@ -1,6 +1,6 @@
-import CodeMirror, { ViewUpdate } from '@uiw/react-codemirror';
-import { mentions } from '@uiw/codemirror-extensions-mentions';
+import CodeMirror, { EditorView, oneDark, ViewUpdate } from '@uiw/react-codemirror';
 import { javascript } from '@codemirror/lang-javascript';
+import React, { useRef, useState } from 'react';
 
 type JavaScriptEditorProps = {
   mentions: {
@@ -26,13 +26,46 @@ const onChange = (value: string, viewUpdate: ViewUpdate) => {
 
 
 const JavaScriptEditor = () => {
+  const editorRef = useRef(null);
+  const [value, setValue] = useState('');
+
+
+  const runCode = () => {
+    const lines = value.split('\n');
+    let i = 0;
+    const next = () => {
+      if (i < lines.length) {
+        try {
+          eval(lines[i]);
+        } catch (error) {
+          console.error(`Error at line ${i + 1}: ${error}`);
+        }
+        i++;
+        setTimeout(next, 0);
+      }
+    };
+    next();
+  };
+
   return (
-    <CodeMirror
-      theme={'dark'}
-      height="300px"
-      extensions={[javascript({ jsx: true, typescript: true }), mentions(custom)]}
-      onChange={onChange}
-    />
+    <div>
+      <CodeMirror
+        height="300px"
+        value={value}
+        theme={oneDark}
+        extensions={[
+          javascript({ jsx: true }),
+          EditorView.lineWrapping,
+          EditorView.updateListener.of((update) => {
+            if (update.docChanged) {
+              setValue(update.state.doc.toString());
+            }
+          }),
+        ]}
+        ref={editorRef}
+      />
+      <button onClick={runCode}>Run Code</button>
+    </div>
   );
 };
 
